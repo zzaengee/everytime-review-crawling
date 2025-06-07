a7122c95726ee9a9f0a5710a7d2e1edadc5a5458
zzaengee <odds2698@naver.com>
2025-06-07 20:33:00 +0900

    WebConfig.java

---
+++ src/main/java/com/datascience/everytime/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.datascience.everytime.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // 모든 URL 경로에 대해
+                        .allowedOrigins("*") // 모든 origin 허용 (프론트 개발 중엔 이게 편해!)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
